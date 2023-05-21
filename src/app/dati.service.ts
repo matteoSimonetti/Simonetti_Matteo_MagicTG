c3d2dd14e59943a734dcaff5b13c3806a3b23c80
matteo.simonetti <matteo.simonetti@marconirovereto.it>
2023-05-21 21:12:58 +0200

    creazione e messa a punto di altri filtri

--- src/app/dati.service.ts
+++ src/app/dati.service.ts
@@ -1,24 +1,39 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DatiService {
   datiCreati = new EventEmitter<any>();
-  data:any = [];
+  data: any = [];
   page = 1;
-  name = "";
-  constructor(private http: HttpClient) {  }
-  
-  getData(){
-    const url = "https://api.magicthegathering.io/v1/cards?page=" + this.page +"&pageSize=25" + "&name=" + this.name;
-    this.http.get(url).subscribe((res)=>{
-      this.data = res;
-      //emetto un evento
-      this.datiCreati.emit(this.data);
-      console.log(this.page, 'pagina')
-    })
-  }
+  name = '';
+  color = '';
+  type = '';
+  constructor(private http: HttpClient) {}
 
+  getData() {
+    const url ='https://api.magicthegathering.io/v1/cards?page=' 
+    + this.page + '&pageSize=25' + '&name=' + this.name + '&colors=' + this.color + '&type=' + this.type;
+    console.log(url);
+    this.http.get(url).subscribe(
+      (res) => {
+        this.data = res;
+        //emetto un evento
+        this.datiCreati.emit(this.data);
+        console.log(this.page, 'pagina');
+      },
+      //gestisco errori
+      (error) => {
+        if (error.status === 500) {
+          console.warn('gestendo errore 500');
+          this.getData();
+        } else {
+          // Gestisco altri tipi di errori
+          console.error(error);
+        }
+      }
+    );
+  }
 }
--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatiService } from '../dati.service';
 
 @Component({
   selector: 'app-filters',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./filters.component.css'],
 })
 export class FiltersComponent {
+  constructor(private servizioDati: DatiService) {}
+
+  //filtri per colore
   isCheckedBlack: boolean = false;
   isCheckedWhite: boolean = false;
   isCheckedBlue: boolean = false;
@@ -13,37 +17,127 @@ export class FiltersComponent {
   isCheckedRed: boolean = false;
   black() {
     if (this.isCheckedBlack) {
-      // La checkbox è stata spuntata
-      console.log('Checkbox selezionata');
-      // Altri comandi o azioni
-    } else {}
+      this.servizioDati.color += 'B,';
+    } else if (this.servizioDati.color.includes('B,')) {
+      this.servizioDati.color = this.servizioDati.color.replace('B,', '');
+    }
+    this.servizioDati.getData();
   }
   white() {
     if (this.isCheckedWhite) {
-      // La checkbox è stata spuntata
-      console.log('Checkbox selezionata');
-      // Altri comandi o azioni
-    } else {}
+      this.servizioDati.color += 'W,';
+    } else if (this.servizioDati.color.includes('W,')) {
+      this.servizioDati.color = this.servizioDati.color.replace('W,', '');
+    }
+    this.servizioDati.getData();
   }
   blue() {
     if (this.isCheckedBlue) {
-      // La checkbox è stata spuntata
-      console.log('Checkbox selezionata');
-      // Altri comandi o azioni
-    } else {}
+      this.servizioDati.color += 'U,';
+    } else if (this.servizioDati.color.includes('U,')) {
+      this.servizioDati.color = this.servizioDati.color.replace('U,', '');
+    }
+    this.servizioDati.getData();
   }
   green() {
     if (this.isCheckedGreen) {
-      // La checkbox è stata spuntata
-      console.log('Checkbox selezionata');
-      // Altri comandi o azioni
-    } else {}
+      this.servizioDati.color += 'G,';
+    } else if (this.servizioDati.color.includes('G,')) {
+      this.servizioDati.color = this.servizioDati.color.replace('G,', '');
+    }
+    this.servizioDati.getData();
   }
   red() {
     if (this.isCheckedRed) {
-      // La checkbox è stata spuntata
-      console.log('Checkbox selezionata');
-      // Altri comandi o azioni
-    } else {}
+      this.servizioDati.color += 'R,';
+    } else if (this.servizioDati.color.includes('R,')) {
+      this.servizioDati.color = this.servizioDati.color.replace('R,', '');
+    }
+    this.servizioDati.getData();
+  }
+
+  //filtri per tipo
+  isCheckedCreature: boolean = false;
+  isCheckedSorcery: boolean = false;
+  isCheckedInstant: boolean = false;
+  isCheckedEnchantment: boolean = false;
+  isCheckedArtifact: boolean = false;
+  isCheckedLand: boolean = false;
+  creature() {
+    if (this.isCheckedCreature) {
+      this.servizioDati.type = 'Creature';
+      this.isCheckedSorcery = false;
+      this.isCheckedInstant = false;
+      this.isCheckedEnchantment = false;
+      this.isCheckedArtifact = false;
+      this.isCheckedLand = false;
+    } else if ((this.servizioDati.type = 'Creature')) {
+      this.servizioDati.type = this.servizioDati.type.replace('Creature', '');
+    }
+    this.servizioDati.getData();
+  }
+  sorcery() {
+    if (this.isCheckedSorcery) {
+      this.servizioDati.type = 'Sorcery';
+      this.isCheckedCreature = false;
+      this.isCheckedInstant = false;
+      this.isCheckedEnchantment = false;
+      this.isCheckedArtifact = false;
+      this.isCheckedLand = false;
+    } else if ((this.servizioDati.type = 'Sorcery')) {
+      this.servizioDati.type = this.servizioDati.type.replace('Sorcery', '');
+    }
+    this.servizioDati.getData();
+  }
+  instant() {
+    if (this.isCheckedInstant) {
+      this.servizioDati.type = 'Instant';
+      this.isCheckedCreature = false;
+      this.isCheckedSorcery = false;
+      this.isCheckedEnchantment = false;
+      this.isCheckedArtifact = false;
+    } else if ((this.servizioDati.type = 'Instant')) {
+      this.servizioDati.type = this.servizioDati.type.replace('Instant', '');
+    }
+    this.servizioDati.getData();
+  }
+  enchantment(){
+    if (this.isCheckedEnchantment) {
+      this.servizioDati.type = 'Enchantment';
+      this.isCheckedCreature = false;
+      this.isCheckedSorcery = false;
+      this.isCheckedInstant = false;
+      this.isCheckedArtifact = false;
+      this.isCheckedLand = false;
+    } else if ((this.servizioDati.type = 'Enchantment')) {
+      this.servizioDati.type = this.servizioDati.type.replace('Enchantment', '');
+    }
+    this.servizioDati.getData();
+  }
+  artifact(){
+    if (this.isCheckedArtifact) {
+      this.servizioDati.type = 'Artifact';
+      this.isCheckedCreature = false;
+      this.isCheckedSorcery = false;
+      this.isCheckedInstant = false;
+      this.isCheckedEnchantment = false;
+      this.isCheckedLand = false;
+    } else if ((this.servizioDati.type = 'Artifact')) {
+      this.servizioDati.type = this.servizioDati.type.replace('Artifact', '');
+    }
+    this.servizioDati.getData();
+  }
+  land(){
+    if (this.isCheckedLand) {
+      this.servizioDati.type = 'Land';
+      this.isCheckedCreature = false;
+      this.isCheckedSorcery = false;
+      this.isCheckedInstant = false;
+      this.isCheckedEnchantment = false;
+      this.isCheckedArtifact = false;
+    } else if ((this.servizioDati.type = 'Land')) {
+      this.servizioDati.type = this.servizioDati.type.replace('Land', '');
+    }
+    this.servizioDati.getData();
   }
 }
